7d4149c47b922f42c81b1edc51f44cec6a710030
Bryan Lilly <bryanjameslilly@gmail.com>
2021-10-15 18:25:51 +0700

    visitsPerDay line graph

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -27,15 +27,21 @@ const getAnalyticsData = (linkID) => {
     })
     .then(response => response.json())
     .then(json => {
-        console.log(json.data);
+        // console.log(json.data);
+        disposeCharts();
         loadWorldMap(json.data.country_code);
         loadNumberOfClicks(json.data.visits);
         loadLinkUrl(json.data.url, `${baseUrl}/${json.data.short}`);
         loadRefererChart(json.data.referer);
+        loadVisitsPerDay(json.data.visitsPerDay);
     })
     .catch(err => {
         console.log(err);
     });
+}
+
+const disposeCharts = () => {
+    am4core.disposeAllCharts();
 }
 
 const loadWorldMap = (myJSON) => {
@@ -106,7 +112,7 @@ const loadWorldMap = (myJSON) => {
             }
         }
     }
-    console.log(data);
+    // console.log(data);
     worldSeries.data = data;
     // Zoom control
     chart.zoomControl = new am4maps.ZoomControl();
@@ -158,4 +164,92 @@ const loadRefererChart = (refererObj) => {
     })
 
     chart.data = chartData;
+}
+
+const loadVisitsPerDay = (visitsPerDay) => {
+    // console.log(visitsPerDay);
+
+    let chartData = [];
+    if(!visitsPerDay){
+        chartData = [];
+    } else {
+        let datesArr = Object.keys(visitsPerDay);
+        datesArr.forEach(result => {
+            chartData.push({
+                date: result,
+                value: visitsPerDay[result]
+            });
+        });
+    }
+
+
+    // console.log(chartData);
+
+
+    am4core.ready(function() {
+
+        // Themes begin
+        am4core.useTheme(am4themes_animated);
+        // Themes end
+        
+        // Create chart instance
+        var chart = am4core.create("visits-per-day-graph", am4charts.XYChart);
+        
+        // Add data
+        chart.data = chartData;
+        
+        // Set input format for the dates
+        chart.dateFormatter.inputDateFormat = "yyyy-MM-dd";
+        
+        // Create axes
+        var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+        var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        
+        // Create series
+        var series = chart.series.push(new am4charts.LineSeries());
+        series.dataFields.valueY = "value";
+        series.dataFields.dateX = "date";
+        series.tooltipText = "{value}"
+        series.strokeWidth = 2;
+        series.minBulletDistance = 15;
+        
+        // Drop-shaped tooltips
+        series.tooltip.background.cornerRadius = 20;
+        series.tooltip.background.strokeOpacity = 0;
+        series.tooltip.pointerOrientation = "vertical";
+        series.tooltip.label.minWidth = 40;
+        series.tooltip.label.minHeight = 40;
+        series.tooltip.label.textAlign = "middle";
+        series.tooltip.label.textValign = "middle";
+        
+        // Make bullets grow on hover
+        var bullet = series.bullets.push(new am4charts.CircleBullet());
+        bullet.circle.strokeWidth = 2;
+        bullet.circle.radius = 4;
+        bullet.circle.fill = am4core.color("#fff");
+        
+        var bullethover = bullet.states.create("hover");
+        bullethover.properties.scale = 1.3;
+        
+        // Make a panning cursor
+        chart.cursor = new am4charts.XYCursor();
+        chart.cursor.behavior = "panXY";
+        chart.cursor.xAxis = dateAxis;
+        chart.cursor.snapToSeries = series;
+        
+        // Create vertical scrollbar and place it before the value axis
+        chart.scrollbarY = new am4core.Scrollbar();
+        chart.scrollbarY.parent = chart.leftAxesContainer;
+        chart.scrollbarY.toBack();
+        
+        // Create a horizontal scrollbar with previe and place it underneath the date axis
+        chart.scrollbarX = new am4charts.XYChartScrollbar();
+        chart.scrollbarX.series.push(series);
+        chart.scrollbarX.parent = chart.bottomAxesContainer;
+        
+        dateAxis.start = 0.79;
+        dateAxis.keepSelection = true;
+        
+        
+        });
 }
